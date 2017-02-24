852983459eb1b0a1d2b1cb53027b4ee20de8c0ea
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-02-24 09:29:16 -0500

    Quiz 1

---
+++ src/quiz1/Quiz1.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz1;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author ratid6445
+ */
+public class Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create city
+        City kw = new City();
+
+        //create robot
+        Robot karel = new Robot(kw, 4, 0, Direction.EAST);
+
+        //create staircase
+        new Wall(kw, 4, 2, Direction.WEST);
+        new Wall(kw, 4, 2, Direction.NORTH);
+        new Wall(kw, 3, 3, Direction.WEST);
+        new Wall(kw, 3, 3, Direction.NORTH);
+        new Wall(kw, 2, 4, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.NORTH);
+        new Wall(kw, 2, 5, Direction.NORTH);
+        new Wall(kw, 2, 5, Direction.EAST);
+        new Wall(kw, 3, 6, Direction.NORTH);
+        new Wall(kw, 3, 6, Direction.EAST);
+        new Wall(kw, 4, 7, Direction.NORTH);
+        new Wall(kw, 4, 7, Direction.EAST);
+
+        //create lights
+        new Thing(kw, 4, 1);
+        new Thing(kw, 3, 2);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 1, 4);
+
+        //climb staircase and pick up things along the way
+        karel.move();
+        karel.pickThing();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.pickThing();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.pickThing();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.pickThing();
+
+        //descend staircase and put things down along the way
+        karel.move();
+        karel.putThing();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.putThing();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.putThing();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.putThing();
+
+        //move karel to final position
+        karel.turnLeft();
+        karel.move();
+    }
+}
